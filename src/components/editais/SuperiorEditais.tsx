ea5392dcd46d6dae2bbcc374d15a253e9f228241
Gabrielogui <gabrielcsro@gmail.com>
2025-05-14 14:57:17 -0300

    atualizando pop up de add
    > Componentes prontos (select, file-upload, text area)

--- src/components/editais/SuperiorEditais.tsx
+++ src/components/editais/SuperiorEditais.tsx
@@ -1,9 +1,14 @@
+"use client"
+
 import { ChevronRightIcon } from "lucide-react";
 import CategoriaColor from "./CategoriaColor";
 import { Button } from "../ui/button";
 import { Sheet, SheetContent, SheetDescription, SheetFooter, SheetHeader, SheetTitle, SheetTrigger } from "../ui/sheet";
 import { Input } from "../ui/input";
 import { Label } from "@radix-ui/react-label";
+import { Textarea } from "../ui/textarea";
+import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from "../ui/select";
+import { FileUpload } from "../ui/file-upload";
 
 export default function SuperiorEditais () {
 
@@ -28,7 +33,7 @@ export default function SuperiorEditais () {
                     <SheetTrigger>
                         <Button variant={"outline"}>Enviar novo edital</Button> {/* ADICIONAR ÍCONE */}
                     </SheetTrigger>
-                    <SheetContent side="right" className="w-1/2">
+                    <SheetContent side="right" className="w-1/2 max-w-[50%]">
                         <SheetHeader>
                             <SheetTitle className="text-2xl">Adicionar</SheetTitle>
                             <SheetDescription>Edital</SheetDescription>
@@ -36,22 +41,79 @@ export default function SuperiorEditais () {
                     
                         <div className="px-4 space-y-6">
                             <div className="flex flex-row gap-3">
-                                <div>
+                                <div className="w-[85%]">
                                     <Label htmlFor="name">Nome do Edial</Label>
                                     <Input id="name" placeholder="Insira o nome do edital"></Input>
                                 </div>
                                 <div>
-                                    <Label htmlFor="unit">Nome do Edial</Label>
-                                    <Input id="unit" placeholder="Escolha a unidade"></Input>
+                                    <Label htmlFor="unit">Unidade*</Label>
+                                    <Select>
+                                        <SelectTrigger className="">
+                                            <SelectValue placeholder="Selcione a unidade" />
+                                        </SelectTrigger>
+                                        <SelectContent>
+                                            <SelectGroup>
+                                                <SelectLabel>Unidade</SelectLabel>
+                                                <SelectItem value="unit">Unidade01</SelectItem>
+                                                <SelectItem value="unit">Unidade02</SelectItem>
+                                                <SelectItem value="unit">Unidade03</SelectItem>
+                                                <SelectItem value="unit">Unidade04</SelectItem>
+                                                <SelectItem value="unit">Unidade05</SelectItem>
+                                            </SelectGroup>
+                                        </SelectContent>
+                                    </Select>
                                 </div>
                             </div>
-                            <div className=""></div>
-                            <div className=""></div>
-                            <div className=""></div>
+                            <div className="flex flex-row gap-3">
+                                <div>
+                                    <Label htmlFor="tipe">Tipo</Label>
+                                    <Select>
+                                        <SelectTrigger className="">
+                                            <SelectValue placeholder="Selcione o tipo" />
+                                        </SelectTrigger>
+                                        <SelectContent>
+                                            <SelectGroup>
+                                                <SelectLabel>tipo</SelectLabel>
+                                                <SelectItem value="type">tipo01</SelectItem>
+                                                <SelectItem value="type">tipo02</SelectItem>
+                                                <SelectItem value="type">tipo03</SelectItem>
+                                                <SelectItem value="type">tipo04</SelectItem>
+                                                <SelectItem value="type">tipo05</SelectItem>
+                                            </SelectGroup>
+                                        </SelectContent>
+                                    </Select>
+
+                                </div>
+                                <div>
+                                    <Label htmlFor="date">Data</Label>
+                                    <Input id="fate" placeholder="Selecione a data"></Input>
+                                </div>
+                            </div>
+                            <div className="flex flex-row gap-3">
+                                <div>
+                                    <Label htmlFor="responsavel">Responsavel*</Label>
+                                    <Input id="responsavel" placeholder="Informe o responsável"></Input>
+
+                                </div>
+                                <div>
+                                    <Label htmlFor="date">Número do edital*</Label>
+                                    <Input id="fate" placeholder="Informe o número do edital"></Input>
+                                </div>
+                            </div>
+                            <div>
+                                <Label>Descrição</Label>
+                                <Textarea/>
+                            </div>
+                            <div className="">
+                                <Label>Upload do documento*</Label>
+                                <div className="h-16">
+                                    <FileUpload />
+                                </div> 
+                            </div>
                         </div>
                     
-                        <SheetFooter>
-                            oi
+                        <SheetFooter className="flex flex-end justify-end max-w-40">
+                            <Button>Salvar Edital</Button>
                         </SheetFooter>
                     </SheetContent>
                 </Sheet>
