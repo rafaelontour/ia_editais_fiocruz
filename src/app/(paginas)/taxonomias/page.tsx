6520002b96c1ae6fc884dba2196df185d11150a6
Alana Abreu <149194023+N1nh4@users.noreply.github.com>
2025-05-20 20:56:20 -0300

    pequenas mudanças no popup

--- src/app/(paginas)/taxonomias/page.tsx
+++ src/app/(paginas)/taxonomias/page.tsx
@@ -8,11 +8,22 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+  DialogFooter,
+  DialogClose,
+} from "@/components/ui/dialog";
 import { Calendar, ChevronLeft, PencilLine, Plus, Trash } from "lucide-react";
 import { useState } from "react";
 
 interface Ramo {
   nome: string;
+  descricao: string;
 }
 
 interface Taxonomia {
@@ -24,18 +35,18 @@ interface Taxonomia {
 
 
 export default function Taxonomias() {
-  const itens: Taxonomia[] = [
+  const itenss: Taxonomia[] = [
     {
       nome: "Taxonomia 1",
       descricao:
         "Determina que a remuneração em contratos de serviços de TI deve estar vinculada a resultados ou atendimento de níveis de serviço, salvo exceções justificadas.",
       data: "25/02/2025",
       ramos: [
-        { nome: "Ramo 1 Taxonomia 1" },
-        { nome: "Ramo 2 Taxonomia 1" },
-        { nome: "Ramo 3 Taxonomia 1" },
-        { nome: "Ramo 4 Taxonomia 1" },
-        { nome: "Ramo 5 Taxonomia 1" },
+        { nome: "Ramo 1 Taxonomia 1", descricao: "Ramo 1 Taxonomia 1" },
+        { nome: "Ramo 2 Taxonomia 1", descricao: "Ramo 2 Taxonomia 1" },
+        { nome: "Ramo 3 Taxonomia 1", descricao: "Ramo 3 Taxonomia 1" },
+        { nome: "Ramo 4 Taxonomia 1", descricao: "Ramo 4 Taxonomia 1" },
+        { nome: "Ramo 5 Taxonomia 1", descricao: "Ramo 5 Taxonomia 1" },
       ],
     },
     {
@@ -44,8 +55,8 @@ export default function Taxonomias() {
         "Determina que a remuneração em contratos de serviços de TI deve estar vinculada a resultados ou atendimento de níveis de serviço, salvo exceções justificadas.",
       data: "25/02/2025",
       ramos: [
-        { nome: "Ramo 1 Taxonomia 2" },
-        { nome: "Ramo 2 Taxonomia 2" },
+        { nome: "Ramo 1 Taxonomia 2", descricao: "Ramo 1 Taxonomia 2" },
+        { nome: "Ramo 2 Taxonomia 2", descricao: "Ramo 2 Taxonomia 2" },
   
       ],
     },
@@ -58,6 +69,35 @@ export default function Taxonomias() {
   ];
 
   const [taxonomiaSelecionada, setTaxonomiaSelecionada] = useState<Taxonomia | null>(null);
+  const ramoNovo: Ramo = {
+    nome: '',
+    descricao: '',
+
+  }
+
+  const [itens, setItens] = useState<Taxonomia[]>(itenss);
+
+  const handleAddRamo = () => {
+    if (taxonomiaSelecionada) {
+      const novaTaxonomia = {
+        ...taxonomiaSelecionada,
+        ramos: [...taxonomiaSelecionada.ramos || [], ramoNovo],
+      };
+
+      setItens((prevItens) =>
+        prevItens.map((item) =>
+          item.nome === taxonomiaSelecionada.nome ? novaTaxonomia : item
+        )
+      );
+
+      setTaxonomiaSelecionada(novaTaxonomia);
+
+      setOpen(false);
+    }
+  };
+
+
+  const [open, setOpen] = useState(false);
 
   return (
     <div className="flex flex-col w-full">
@@ -69,7 +109,9 @@ export default function Taxonomias() {
           <h1 className="ml-4 font-semibold text-2xl">Gestão de Taxonomia e Ramos</h1>
         </div>
 
-        <button className="mr-4 flex items-center gap-2 bg-red-500 hover:bg-red-600 cursor-pointer hover:shadow-md text-white font-semibold py-2 px-4 rounded-sm">
+        <button 
+          className="mr-4 flex items-center gap-2 bg-red-500 hover:bg-red-600 cursor-pointer hover:shadow-md text-white font-semibold py-2 px-4 rounded-sm"
+        >
           <Plus className="h-5 w-5 " strokeWidth={1.5} />
           <span>Adicionar </span>
         </button>
@@ -112,10 +154,67 @@ export default function Taxonomias() {
             <CardHeader>
               <CardTitle className="flex flex-row justify-between items-center">
                 <h1 className="text-2xl">Ramos</h1>
-                <button className=" flex items-center gap-2 bg-red-500 hover:bg-red-600 cursor-pointer hover:shadow-md text-white font-semibold py-2 px-4 rounded-sm">
-                <Plus className="h-5 w-5 " strokeWidth={1.5} />
-                <span>Adicionar Ramo </span>
-                </button>
+                <Dialog open={open} onOpenChange={setOpen}>
+                  <DialogTrigger asChild >
+                    <button 
+                        className=" flex items-center gap-2 bg-red-500 hover:bg-red-600 cursor-pointer hover:shadow-md text-white font-semibold py-2 px-4 rounded-sm"
+                      
+                    >
+                      <Plus className="h-5 w-5 " strokeWidth={1.5} />
+                      Adicionar Ramos
+                    </button>
+                  
+                  </DialogTrigger>
+                  <DialogContent >
+                    <DialogHeader>
+                      <DialogTitle>Adicionar ramo</DialogTitle>
+                      <DialogDescription>
+                        Preencha os campos abaixo para adicionar um novo ramo.
+                      </DialogDescription>
+                    </DialogHeader>
+                    <div className="space-y-4">
+                      <div>
+                        <label htmlFor="nomeRamo" className="block text-sm font-medium text-gray-700">
+                          Nome do Ramo*
+                        </label>
+                        <input
+                          onChange={(e) => ramoNovo.nome = e.target.value}
+                          type="text"
+                          id="nomeRamo"
+                          className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-red-500 focus:border-red-500 sm:text-sm"   
+                        />
+                      </div>
+                      <div>
+                          <label htmlFor="descricaoRamo" className="block text-sm font-medium text-gray-700">
+                            Descrição do Ramo*
+                          </label>
+                          <textarea
+                            onChange={(e) => ramoNovo.descricao = e.target.value}
+                            id="descricaoRamo"
+                            placeholder="Digite uma descrição para o ramo"
+                            rows={4}
+                            className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-red-500 focus:border-red-500 sm:text-sm"
+                          />
+                      </div>
+                    </div>
+                    <DialogFooter>
+                      <DialogClose className="px-4 py-2 text-sm font-semibold text-gray-700 bg-gray-100 hover:bg-gray-200 rounded-md">
+                          Cancelar
+                      </DialogClose>
+                      
+                      <button 
+                        className="px-4 py-2 text-sm font-semibold text-white bg-red-500 hover:bg-red-600 rounded-md"
+                        type="submit"
+                        onClick={handleAddRamo}
+                      >
+                        Adicionar
+                      </button>
+
+                      
+                    </DialogFooter>
+
+                  </DialogContent>
+                </Dialog>
               </CardTitle>
 
             </CardHeader>
