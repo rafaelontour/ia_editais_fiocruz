b895ea2f82f80428fa441f8a1323fd43439de1d5
Rafael <rafaelnargolo@outlook.com>
2025-05-23 10:59:11 -0300

    Pop up feito, faltando detalhes do componente FileUpload

--- src/app/(paginas)/fontes/page.tsx
+++ src/app/(paginas)/fontes/page.tsx
@@ -1,14 +1,103 @@
 'use client'
 
 import Masonry from 'react-masonry-css'
-import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTrigger } from "@/components/ui/dialog";
+import { Dialog, DialogClose, DialogContent, DialogDescription, DialogHeader, DialogTrigger } from "@/components/ui/dialog";
 import { DialogTitle } from "@radix-ui/react-dialog";
-import { Plus } from "lucide-react";
+import { Calendar, Edit, Plus, Trash } from "lucide-react";
 import { useState } from "react";
+import { Button } from '@/components/ui/button';
+import { FileUpload } from '@/components/ui/file-upload';
 
 export default function Fontes() {
+    const breakpointColumnsObj = {
+        default: 4,
+        1500: 3,
+        1000: 2,
+        700: 1
+    }
 
-    const [open, setOpen] = useState(false);
+    const itens = [
+        {
+            nome: "Tipificacao 1",
+            lei: "Lei 1",
+            lei_complementar: "Lei Complementar 1",
+            data: "12/12/2022"
+        },
+        {
+            nome: "Tipificacao 2",
+            lei: "Lei 2",
+            lei_complementar: "Lei Complemenmplementar 9Lei Complementar 9Lei Complementar 9",
+            data: "8/12/2022"
+        },
+        {
+            nome: "Tipificacao 3",
+            lei: "Lei 3",
+            lei_complementar: "Lei 3 Lei CoComplementar 9Lei Complementar 9Lei Complementar 9Lei Complementar 9",
+            data: "10/12/2020"
+        },
+        {
+            nome: "Tipificacao 4",
+            lei: "Lei 4",
+            lei_complementar: "Lei Complementar 4",
+            data: "20/12/2010"
+        },
+        {
+            nome: "Tipificacao 5",
+            lei: "Lei 5",
+            lei_complementar: "Lei Complementar 5",
+            data: "30/12/2000"
+        },
+        {
+            nome: "Tipificacao 6",
+            lei: "Lei 6",
+            lei_complementar: "Lei Complementar 6 Lei Complementar 9Lei Complementar 9Lei Complementar 9Lei Complementar 9Lei Complementar 9Lei Complementar 9",
+            data: "20/12/1990"
+        },
+        {
+            nome: "Tipificacao 7",
+            lei: "Lei 7",
+            lei_complementar: "Lei Complementar  Lei Complementar 9Le9Lei Complementar 9Lei Complementar 9Lei Complementar 9Lei Complementar 9Lei Complementar 9Lei Complementar 9Lei Complementar 9",
+            data: "20/12/1980"
+        },
+        {
+            nome: "Tipificacao 8",
+            lei: "Lei 8",
+            lei_complementar: "Lei Complementar 8",
+            data: "20/12/1970"
+        },
+        {
+            nome: "Tipificacao 9",
+            lei: "Lei 9",
+            lei_complementar: "Lei Complementar 9 Lei Complementar 9Lei Complementar 9Lei Complementar 9Lei Complementar 9Lei Complementalementar 9Lei Complementar 9",
+            data: "20/12/1960"
+        },
+        {
+            nome: "Tipificacao 10",
+            lei: "Lei 10",
+            lei_complementar: "Lei Complementar 10 Lei Complementar 9Lei Complementar 9Lei Complementar 9Lei Complementar 9Lei Comple",
+            data: "20/12/1950"
+        },
+        {
+            nome: "Tipificacao 11",
+            lei: "Lei 11",
+            lei_complementar: "Lei Complementar 11",
+            data: "20/12/1940"
+        },
+        {
+            nome: "Tipificacao 12",
+            lei: "Lei 12",
+            lei_complementar: "Lei Complementar 12",
+            data: "20/12/1930"
+        },
+        {
+            nome: "Tipificacao 13",
+            lei: "Lei 13",
+            lei_complementar: "Lei Complementar 13",
+            data: "20/12/1920"
+        }
+    ]
+
+    const [openDialogFontes, setOpenDialogFontes] = useState(false);
 
     return (
         <div className="flex flex-col gap-5">
@@ -17,7 +106,7 @@ export default function Fontes() {
                     Fontes
                 </h2>
 
-                <Dialog open={open} onOpenChange={setOpen}>
+                <Dialog open={openDialogFontes} onOpenChange={setOpenDialogFontes}>
                     <DialogTrigger>
                         <div
                             style={{ boxShadow: "0 0 3px rgba(0,0,0,.5)"}}
@@ -36,88 +125,119 @@ export default function Fontes() {
                     <DialogContent>
 
                         <DialogHeader>
-                            <DialogTitle>
-                                Adicionar Fonte
+                            <DialogTitle className="text-3xl font-bold">
+                                Adicionar Fonte à base de dados
                             </DialogTitle>
 
-                            <DialogDescription>
-                                Descrição
+                            <DialogDescription className="text-md pb-4">
+                                Adicione os dados da fonte
                             </DialogDescription>
 
                         </DialogHeader>
+
+                        <form className="flex text-lg flex-col gap-4">
+                            <p className="flex flex-col gap-2">
+                                <label htmlFor="nomeFonte" className="">Nome da fonte</label>
+                                <input type="text" id="nomeFonte" className="border-2 border-gray-300 rounded-md p-2 w-full" />
+                            </p>
+
+                            <p className="flex flex-col gap-2">
+                                <label htmlFor="descricaoFonte" className="">Descrição da fonte</label>
+                                <input type="text" id="descricaoFonte" className="border-2 border-gray-300 rounded-md p-2 w-full" />
+                            </p>
+
+                            <p>
+                                Upload do documento (opcional)</p>
+                            <FileUpload />
+
+                        </form>
+
+
+                        <div className="flex justify-end gap-4 mt-4">
+                            <DialogClose
+                                className={`
+                                    transition ease-in-out text-white
+                                    rounded-md px-3 bg-vermelho
+                                    hover:cursor-pointer
+                                    hover:scale-110 active:scale-100
+                                `}
+                                style={{ boxShadow: "0 0 3px rgba(0,0,0,.5)"}}
+                            >
+                                Cancelar
+                            </DialogClose>
+                            
+                            <Button
+                                className={`
+                                    flex bg-verde hover:bg-verde
+                                    text-white hover:cursor-pointer
+                                    hover:scale-110 active:scale-100
+                                `}
+                                style={{ boxShadow: "0 0 3px rgba(0,0,0,.5)"}}
+                                onClick={() => setOpenDialogFontes(false)}
+                            >
+                                Salvar
+                            </Button>
+                        </div>
                     </DialogContent>
                 </Dialog>
             </div>
 
             <Masonry
-                breakpointCols={3} 
-                className="bg-blue-400" 
-                columnClassName="my-masonry-grid_column"
+                breakpointCols={breakpointColumnsObj}
+                columnClassName="pl-4"
+                className={'flex -ml-4 w-auto'}
             >
-                <div className="bg-red-100 w-[300px] h-auto">
-                    teste
-                </div><div className="bg-red-100 w-[300px] h-auto">
-                    teste
-                </div>
-                <div className="bg-red-100 w-[300px] h-auto">
-                    teste
-                </div>
-                <div className="bg-red-100 w-[300px] h-auto">
-                    teste
-                </div>
-                <div className="bg-red-100 w-[300px] h-auto">
-                    teste
-                </div>
-                <div className="bg-red-100 w-[300px] h-auto">
-                    teste
-                </div>
-                <div className="bg-red-100 w-[300px] h-auto">
-                    teste
-                </div>
-                <div className="bg-red-100 w-[300px] h-auto">
-                    teste
-                </div>
-                <div className="bg-red-100 w-[300px] h-auto">
-                    teste
-                </div>
-                <div className="bg-red-100 w-[300px] h-auto">
-                    teste
-                </div>
-                <div className="bg-red-100 w-[300px] h-auto">
-                    teste
-                </div>
-                <div className="bg-red-100 w-[300px] h-auto">
-                    teste
-                </div>
-                <div className="bg-red-100 w-[300px] h-auto">
-                    teste
-                </div>
-                <div className="bg-red-100 w-[300px] h-auto">
-                    teste
-                </div><div className="bg-red-100 w-[300px] h-auto">
-                    teste
-                </div>
-                <div className="bg-red-100 w-[300px] h-auto">
-                    teste
-                </div>
-                <div className="bg-red-100 w-[300px] h-auto">
-                    teste
-                </div>
-                <div className="bg-red-100 w-[300px] h-auto">
-                    teste
-                </div>
-                <div className="bg-red-100 w-[300px] h-auto">
-                    teste
-                </div>
-                <div className="bg-red-100 w-[300px] h-auto">
-                    teste
-                </div>
-                <div className="bg-red-100 w-[300px] h-auto">
-                    teste
-                </div>
-                <div className="bg-red-100 w-[300px] h-auto">
-                    teste
-                </div>
+                {itens.map((tipificacao, index) => (
+                    <div
+                        style={{ boxShadow: "0 0 5px rgba(0,0,0,.3)"}} 
+                        key={index}
+                        className="
+                            flex flex-col gap-2 rounded-md p-4 mb-4
+                            hover:scale-105 transition ease-in-out duration-100
+                            min-w-[250px]
+                        "
+                    >
+                        <div className="flex flex-col gap-2">
+                            <h2 className="text-xl font-semibold">{tipificacao.nome}</h2>
+                            <p className={`bg-verde py-1 px-2 text-white rounded-md border-2 border-gray-300 w-fit text-sm`}>
+                            Lei: {tipificacao.lei}
+                            </p>
+                            <p className={`bg-verde py-1 px-2 text-white rounded-md border-2 border-gray-300 break-words text-sm`}>
+                            Lei Complementar: {tipificacao.lei_complementar}
+                            </p>
+                        </div>
+                        <div className="flex justify-between items-center mt-3">
+                            <p className="flex items-center gap-2 text-sm text-gray-400">
+                                <Calendar size={16} />
+                                <span>{tipificacao.data}</span>
+                            </p>
+                            <div className="flex gap-3">
+                                <Button
+                                    className={`
+                                        h-8 w-8 hover:cursor-pointer hover:scale-110 active:scale-100
+                                        bg-branco hover:bg-branco
+                                    `}
+                                    style={{ boxShadow: "0 0 3px rgba(0,0,0,.5)"}}
+                                    size={"icon"}
+                                    
+                                >
+                                    <Edit color="black" />
+                                </Button>
+
+                                <Button
+                                    className={`
+                                        h-8 w-8 bg-vermelho hover:bg-vermelho
+                                        hover:cursor-pointer hover:scale-110 active:scale-100
+                                    `}
+                                    style={{ boxShadow: "0 0 3px rgba(0,0,0,.5)"}}
+                                    size={"icon"}
+                                >
+                                    <Trash />
+                                </Button>
+                            </div>
+                        </div>
+                    </div>
+                ))}
             </Masonry>
         </div>
     )
