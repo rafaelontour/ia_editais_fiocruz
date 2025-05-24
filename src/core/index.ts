24423e0f5d4ed9845573b62106ac7148f526d00a
Rafael <rafaelnargolo@outlook.com>
2025-05-24 15:42:37 -0300

    index.ts do core

---
+++ src/core/index.ts
@@ -0,0 +1,6 @@
+import Fonte from "./fonte/Fonte";
+import Tipificacao from "./tipificacao/Tipificacao";
+import { Ramo } from "./ramo";
+import Taxonomia from "./taxonomia/Taxonomia";
+
+export type { Fonte, Tipificacao, Ramo, Taxonomia };
