931977f32bd13654fac681cbe2fcc80ba98c7a7c
Rafael <rafaelnargolo@outlook.com>
2025-06-07 19:27:29 -0300

    Exportando usuário

--- src/core/index.ts
+++ src/core/index.ts
@@ -3,5 +3,13 @@ import Tipificacao from "./tipificacao/Tipificacao";
 import Ramo from "./ramo/Ramo";
 import Taxonomia from "./taxonomia/Taxonomia";
 import Edital from "./edital/Edital"
+import Usuario from "./usuario/Usuario";
 
-export type { Fonte, Tipificacao, Ramo, Taxonomia, Edital };
+export type {
+    Fonte,
+    Tipificacao,
+    Ramo,
+    Taxonomia,
+    Edital,
+    Usuario
+};
