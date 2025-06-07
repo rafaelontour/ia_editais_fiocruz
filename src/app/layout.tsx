1ca8747fc509f6bade91b554657659ecde25771c
Rafael <rafaelnargolo@outlook.com>
2025-06-07 20:42:26 -0300

    Adicionando o provider de usuário logado

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,26 +1,35 @@
-import type { Metadata } from "next";
-import "@/app/globals.css";
+'use client'
 
-export const metadata: Metadata = {
-  title: "IA Editais",
-  description: "Análise inteligente de editais com inteligência artificial.",
-  icons: {
-    icon: "/favicon.png",
-  }
-};
+import "@/app/globals.css";
+import { UsuarioContextoProvider } from "@/data/context/UsuarioContext";
+import { useState } from "react";
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
+  const [teste, setTeste] = useState<boolean>(false)
   
   return (
     <html lang="pt-BR">
       <body
         className={`antialiased bg-[#F5F5F5] `}
       >
-        {children}
+        <UsuarioContextoProvider>
+          {
+            teste
+            ?
+            <div>
+              Teste funcionando
+            </div>
+            :
+            <div>
+              {children}
+            </div>
+          }
+        </UsuarioContextoProvider>
       </body>
     </html>
   );
