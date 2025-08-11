b7c92dd32f15975c9d6d8857819359023e213344
Gabrielogui <gabrielcsro@gmail.com>
2025-08-11 10:35:46 -0300

    feat: Criação do Dialog para adicionar usuário
    > Interface com 'open' e 'onOpenChange' para abrir o dialog sem precisar do trigger

---
+++ src/components/usuario/AdicionarUsuario.tsx
@@ -0,0 +1,23 @@
+import { DialogContent, DialogTitle } from "@radix-ui/react-dialog";
+import { Dialog, DialogHeader } from "../ui/dialog";
+import { User } from "lucide-react";
+
+interface AdicionarUsuarioProps {
+    open: boolean;
+    onOpenChange: (open: boolean) => void;
+}
+
+export default function AdicionarUsuario ({open, onOpenChange}:AdicionarUsuarioProps) {
+    return(
+        <Dialog open={open} onOpenChange={onOpenChange}>
+            <DialogContent>
+                <DialogHeader>
+                    <DialogTitle>
+                        <User />
+                        Adicionar Usuário
+                    </DialogTitle>
+                </DialogHeader>
+            </DialogContent>
+        </Dialog>     
+    );
+}
