cccefb901bad19cdbc5edc56df1b2e36a0b43a69
Gabrielogui <gabrielcsro@gmail.com>
2025-08-11 09:48:45 -0300

    refactor: Mudando nome do título da página + tamanho do botão

--- src/app/(paginas)/(public)/atribuirCargo/page.tsx
+++ src/app/(paginas)/(public)/atribuirCargo/page.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button";
+import { Label } from "@/components/ui/label";
 import { ChevronLeft, Plus } from "lucide-react";
 
 export default function AtribuirCargo () {
@@ -8,12 +9,14 @@ export default function AtribuirCargo () {
             <div className="flex flex-row justify-between w-full">
                 <div className="flex flex-row gap-2 items-center">
                     <Button variant={"outline"} size={"icon"} className="cursor-pointer"><ChevronLeft /></Button>
-                    <h2 className="text-2xl font-semibold">Atribuir Cargo</h2>
+                    <h2 className="text-2xl font-semibold">Gerenciar Usuários</h2>
                 </div>
                 <div>
-                    <Button variant={"outline"} size={"icon"}
-                        className="bg-vermelho text-white cursor-pointer hover:scale-105 hover:bg-vermelho hover:text-white transition-all"
-                        ><Plus />
+                    <Button variant={"outline"}
+                        className="bg-vermelho text-white items-center cursor-pointer hover:scale-105 hover:bg-vermelho hover:text-white transition-all"
+                        >
+                        <Plus />
+                        <Label>Adicionar Usuário</Label>
                     </Button>
                 </div>
             </div>
