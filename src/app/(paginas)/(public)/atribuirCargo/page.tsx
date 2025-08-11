c7e997d1abf86c39340f72b055c4fe57427e68b8
Gabrielogui <gabrielcsro@gmail.com>
2025-08-11 08:01:57 -0300

    style: Menu superior de atribuir cargo

--- src/app/(paginas)/(public)/atribuirCargo/page.tsx
+++ src/app/(paginas)/(public)/atribuirCargo/page.tsx
@@ -1,7 +1,22 @@
+import { Button } from "@/components/ui/button";
+import { ChevronLeft, Plus } from "lucide-react";
+
 export default function AtribuirCargo () {
     return(
         <div>
-            Atribuir Cargo
+            {/* MENU SUPERIOR - ATRIBUIR CARGO */}
+            <div className="flex flex-row justify-between w-full">
+                <div className="flex flex-row gap-2 items-center">
+                    <Button variant={"outline"} size={"icon"} className="cursor-pointer"><ChevronLeft /></Button>
+                    <h2 className="text-2xl font-semibold">Atribuir Cargo</h2>
+                </div>
+                <div>
+                    <Button variant={"outline"} size={"icon"}
+                        className="bg-vermelho text-white cursor-pointer hover:scale-105 hover:bg-vermelho hover:text-white transition-all"
+                        ><Plus />
+                    </Button>
+                </div>
+            </div>
         </div>
     );
 }
