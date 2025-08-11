262cb440641c771cb69c5010117fa166b8aca06d
Gabrielogui <gabrielcsro@gmail.com>
2025-08-11 11:23:39 -0300

    style: Input e Select no componente de adicionar um usuário

--- src/components/usuario/AdicionarUsuario.tsx
+++ src/components/usuario/AdicionarUsuario.tsx
@@ -1,7 +1,9 @@
-
-import { Dialog, DialogContent, DialogHeader, DialogTitle } from "@/components/ui/dialog";
-import { User } from "lucide-react";
-
+import { Dialog, DialogClose, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle } from "@/components/ui/dialog";
+import { UserPlus } from "lucide-react";
+import { Label } from "../ui/label";
+import { Input } from "../ui/input";
+import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from "../ui/select";
+import { Button } from "../ui/button";
 
 interface AdicionarUsuarioProps {
     open: boolean;
@@ -13,11 +15,60 @@ export default function AdicionarUsuario ({open, onOpenChange}:AdicionarUsuarioP
         <Dialog open={open} onOpenChange={onOpenChange}>
             <DialogContent>
                 <DialogHeader>
-                    <DialogTitle>
-                        <User />
-                        Adicionar Usuário
-                    </DialogTitle>
+                    <div className="flex flex-row gap-2 items-center">
+                        <UserPlus size={20}/>
+                        <DialogTitle>Adicionar Usuário</DialogTitle>
+                    </div>
+                    <DialogDescription>Adicine um usuário</DialogDescription>
                 </DialogHeader>
+                <div className="grid grid-cols-2 gap-3">
+                    <div className="flex flex-col gap-1">
+                        <Label>Nome</Label>
+                        <Input placeholder="Insira o nome"/>
+                    </div>
+                    <div className="flex flex-col gap-1">
+                        <Label>Email</Label>
+                        <Input placeholder="Insira o email"/>
+                    </div>
+                    <div className="flex flex-col gap-1">
+                        <Label>Unidade</Label>
+                        <Select>
+                            <SelectTrigger className="w-full">
+                                <SelectValue placeholder="Selecione a unidade" />
+                            </SelectTrigger>
+                            <SelectContent >
+                                <SelectGroup>
+                                    <SelectLabel>Unidade</SelectLabel>
+                                    {Array.from({length:5}).map((_, i) => (
+                                        <SelectItem key={i} value="unit">Unidade01</SelectItem>
+                                    ))}
+                                </SelectGroup>
+                            </SelectContent>
+                        </Select>
+                    </div>
+                    <div className="flex flex-col gap-1">
+                        <Label>Perfil</Label>
+                        <Select>
+                            <SelectTrigger className="w-full">
+                                <SelectValue placeholder="Selecione o perfil" />
+                            </SelectTrigger>
+                            <SelectContent >
+                                <SelectGroup>
+                                    <SelectLabel>Perfil</SelectLabel>
+                                    {Array.from({length:5}).map((_, i) => (
+                                        <SelectItem key={i} value="unit">Perfil01</SelectItem>
+                                    ))}
+                                </SelectGroup>
+                            </SelectContent>
+                        </Select>
+                    </div>
+                </div>
+                <DialogFooter>
+                    <DialogClose asChild>
+                        <Button variant="outline" className="cursor-pointer">Cancelar</Button>
+                    </DialogClose>
+                    <Button type="submit" className="bg-verde hover:bg-green-900 cursor-pointer">Adicionar</Button>
+                </DialogFooter>
             </DialogContent>
         </Dialog>     
     );
