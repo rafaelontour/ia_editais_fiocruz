97e79963416c187aeb4fce71b60dfecbc9271be5
verttb <alyssonoliveira456@gmail.com>
2025-08-14 14:03:22 -0300

    refactor: mudando getToken para POST e updateUsuario movido para o service de usuario

--- src/service/auth.ts
+++ src/service/auth.ts
@@ -1,64 +1,19 @@
 const urlBase: string | undefined = process.env.NEXT_PUBLIC_URL_BASE
 
 
-async function postUser(name : string, password: string, email: string) {
-    try{
-        const url = `${urlBase}/users `;
 
-        const res = await fetch(url, {
-            method: "POST",
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                name,password, email
-            })
-
-        });
-
-        return res.status
-
-    }catch(e){
-        console.error("Erro ao tentar criar usuário: ", e);
-    }
-}
-
-async function getToken(email: string, password: string) {
+export async function getToken(formData:any) {
      try{
         const url = `${urlBase}/token `;
 
         const res = await fetch(url, {
-            method: "GET",
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                password, email
-            })
+            method: "POST",
+            body: formData
         });
       const data = await res.json()
-     return data;
+      return data;
     }catch(e){
         console.error("Erro ao tentar criar usuário: ", e);
     }
 }
 
-// async function updateUser(email: string, password: string) {
-//      try{
-//         const url = `${urlBase}/token `;
-
-//         const res = await fetch(url, {
-//             method: "GET",
-//             headers: {
-//                 'Content-Type': 'application/json',
-//             },
-//             body: JSON.stringify({
-//                 password, email
-//             })
-//         });
-//       const data = await res.json()
-//      return data;
-//     }catch(e){
-//         console.error("Erro ao tentar criar usuário: ", e);
-//     }
-// }
--- src/service/usuario.ts
+++ src/service/usuario.ts
@@ -1,4 +1,5 @@
 import { Usuario } from "@/core";
+import { NivelAcesso } from "@/core/enum/nivelAcessoEnum";
 
 
 const urlBase = process.env.NEXT_PUBLIC_URL_BASE
@@ -9,10 +10,12 @@ async function getUsuario(): Promise<Usuario | null> {
 
 async function deleteUsuario(usuarioId: string) {
      try{
+        const token = localStorage.getItem("token")
         const res = await fetch(`${urlBase}/user/${usuarioId}`, {
             method: "DELETE",
+            
              headers: {
-                "content-type": "application/json"
+                "Authorization": `Bearer ${token}`
             },
         });
 
@@ -27,7 +30,53 @@ async function deleteUsuario(usuarioId: string) {
     }
 }
 
+async function updateUser(usuarioId: string, email:string, username: string, access_level : string) {
+     try{
+        const url = `${urlBase}/user/${usuarioId} `;
+
+        const res = await fetch(url, {
+            method: "PUT",
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${localStorage.getItem("token")}`
+            },
+            body: JSON.stringify({
+                id: usuarioId, email, username, access_level
+            })
+        });
+      const data = await res.json()
+     return data;
+    }catch(e){
+        console.error("Erro ao tentar criar usuário: ", e);
+    }
+}
+
+
+async function postUser(name : string, password: string, email: string) {
+    try{
+        const url = `${urlBase}/users `;
+
+        const res = await fetch(url, {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                name,password, email
+            })
+
+        });
+
+        return res.status
+
+    }catch(e){
+        console.error("Erro ao tentar criar usuário: ", e);
+    }
+}
+
 export {
     getUsuario,
-    deleteUsuario
+    deleteUsuario,
+    updateUser,
+    postUser
 }
