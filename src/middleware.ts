99f53acabbd88ebdd3c5f201a7d7eade2a3d84db
Rafael <rafaelnargolo@outlook.com>
2025-08-18 17:25:08 -0300

    refactor: Tirando comentário pra teste

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,6 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
-  console.log("Rodou em: ", request.nextUrl.pathname);
   const token = request.cookies.get("access_token")?.value;
 
   if (!token) {
