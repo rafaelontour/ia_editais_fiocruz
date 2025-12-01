ebd7d4393129ea1b273ca9a33bd0e69e6651c6c7
Rafael <rafaelnargolo@outlook.com>
2025-12-01 00:06:42 -0300

    configurações de eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,38 +1,11 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-import nextPlugin from "@next/eslint-plugin-next"; // <<< IMPORTA O PLUGIN
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
 export default [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-
-  {
-    plugins: {
-      "@next/next": nextPlugin, // <<< REGISTRA O PLUGIN PARA PARAR OS ERROS
-    },
-
-    rules: {
-      "@typescript-eslint/no-unused-vars": "off",
-      "@typescript-eslint/no-explicit-any": "off",
-      "@typescript-eslint/no-non-null-asserted-optional-chain": "off",
-      "react-hooks/exhaustive-deps": "warn",
-      "prefer-const": "warn",
-    },
-  },
-
   {
     ignores: [
-      ".next/**",
-      "node_modules/**",
-      "dist/**",
-      "out/**"
-    ]
-  }
+      "cypress/**",        // ignora tudo do cypress
+      ".next/**",          // ignora build
+      "node_modules/**"    // padrão, mas sempre bom garantir
+    ],
+  },
+
+  // restante das tuas configs aqui...
 ];
