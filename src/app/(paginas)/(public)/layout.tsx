52acbc523fea575442fba55587b66bd1b8605f09
Alana Abreu <149194023+N1nh4@users.noreply.github.com>
2026-03-14 09:06:55 -0300

    mudança no layout principal para ficar igual ao layout adm no icone do avatar do perfil

--- src/app/(paginas)/(public)/layout.tsx
+++ src/app/(paginas)/(public)/layout.tsx
@@ -1,12 +1,16 @@
-'use client'
+"use client";
 
 import Cabecalho from "@/components/Cabecalho";
 import { useEffect, useState } from "react";
 import { usePathname } from "next/navigation";
 import useUsuario from "@/data/hooks/useUsuario";
-import { Tooltip, TooltipContent, TooltipTrigger } from "@/components/ui/tooltip";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 import Link from "next/link";
-import {  UserIcon } from "lucide-react";
+import { UserIcon } from "lucide-react";
 import { motion } from "motion/react";
 
 export default function RootLayout({
@@ -30,7 +34,7 @@ export default function RootLayout({
 
   const title = titulosMap[pathname] || "IAEditais";
 
-  const urlBase = process.env.NEXT_PUBLIC_URL_BASE
+  const urlBase = process.env.NEXT_PUBLIC_URL_BASE;
 
   // Atualiza o título dinamicamente no client (quando navega via Link)
   useEffect(() => {
@@ -39,30 +43,33 @@ export default function RootLayout({
 
   return (
     <div className="flex flex-col overflow-hidden w-full scrollbar-hidden">
-    
       <Cabecalho />
 
       <div className="overflow-hidden">
-
         <div className="flex h-[calc(100vh-4rem)]">
-
           <motion.div
             layout
             className="flex top-14 left-0 h-[calc(100vh-4rem)] bg-zinc-100 z-10 overflow-hidden"
-            style={{ boxShadow: "4px 0 2px rgba(0, 0, 0, .12)"}}
-            animate={{ width: barraLateralAberta ? 260 : 60, transition: { duration: 0.2 } }}
+            style={{ boxShadow: "4px 0 2px rgba(0, 0, 0, .12)" }}
+            animate={{
+              width: barraLateralAberta ? 260 : 60,
+              transition: { duration: 0.2 },
+            }}
             transition={{ type: "spring", stiffness: 300, damping: 30 }}
           >
             <div className={`w-[300px] flex flex-col justify-between`}>
               <nav className={``}>
                 <ul>
-                  {items.map((item) => (
+                  {items.map((item) =>
                     barraLateralAberta ? (
                       <li key={item.url}>
-                        <Link href={item.url}
+                        <Link
+                          href={item.url}
                           className={`flex items-center gap-3 px-3 py-0.5`}
                         >
-                          <span className={`flex ${barraLateralAberta ? "w-full" : "w-fit"} hover:bg-vermelho hover:text-white text-sm mb-1 items-center gap-2 p-2 rounded-md ${pathname === item.url ? "bg-vermelho font-bold text-white" : "bg-zinc-300"}`}>
+                          <span
+                            className={`flex ${barraLateralAberta ? "w-full" : "w-fit"} hover:bg-vermelho hover:text-white text-sm mb-1 items-center gap-2 p-2 rounded-md ${pathname === item.url ? "bg-vermelho font-bold text-white" : "bg-zinc-300"}`}
+                          >
                             <item.icon size={18} />
                             {barraLateralAberta && <span>{item.title}</span>}
                           </span>
@@ -71,10 +78,15 @@ export default function RootLayout({
                     ) : (
                       <Tooltip key={item.url}>
                         <TooltipTrigger asChild>
-                          <Link className="flex items-center gap-3 px-3 py-0.5" href={item.url}>
-                            <span className={`flex ${barraLateralAberta ? "w-full" : "w-fit"} hover:bg-vermelho hover:text-white text-sm mb-1 items-center gap-2 p-2 rounded-md ${pathname === item.url ? "bg-vermelho font-bold text-white" : "bg-zinc-300"}`}>
-                            <item.icon size={18} />
-                          </span>
+                          <Link
+                            className="flex items-center gap-3 px-3 py-0.5"
+                            href={item.url}
+                          >
+                            <span
+                              className={`flex ${barraLateralAberta ? "w-full" : "w-fit"} hover:bg-vermelho hover:text-white text-sm mb-1 items-center gap-2 p-2 rounded-md ${pathname === item.url ? "bg-vermelho font-bold text-white" : "bg-zinc-300"}`}
+                            >
+                              <item.icon size={18} />
+                            </span>
                           </Link>
                         </TooltipTrigger>
 
@@ -82,58 +94,59 @@ export default function RootLayout({
                           {item.title}
                         </TooltipContent>
                       </Tooltip>
-                    )
-                  ))}
+                    ),
+                  )}
                 </ul>
               </nav>
-              
-              
 
-              
-
-                {
-                  usuario?.id && (
-                    <div className="flex items-center gap-4 w-full p-4 pl-3 pb-4">
-                      <div className={`flex items-center justify-center h-9 min-w-8.5 max-w-full rounded-full gap-3`}>
-                        <div className="bg-verde rounded-full p-0.5">
-                          {
-                            usuario && (
-                              usuario.icon ? (
-                                <img src={urlBase + usuario.icon.file_path} className={`w-8 h-8 rounded-full`} />
-                              ) : (
-                                <UserIcon size={18} />
-                              )
-                            )
-                          }
-                        </div>
-
-                        <motion.div
-                          initial={false}
-                          animate={{
-                            opacity: barraLateralAberta ? 1 : 0,
-                            pointerEvents: barraLateralAberta ? "auto" : "none",
-                            transition: {
-                              delay: barraLateralAberta ? 0.3 : 0,
-                              duration: 0.2,
-                              ease: "easeOut"
-                            }
-                          }}
-                          exit={{
-                            display: barraLateralAberta ? "flex" : "none"
-                          }}
-                          style={{
-                            display: barraLateralAberta ? "flex" : "none"
-                          }}
-                          className={`flex flex-col gap-0 min-w-[120px]`}
-                        >
-                          <span className="text-sm font-bold">{usuario?.username}</span>
-                          <Link className="h-fit" href="/adm/meu-perfil"><span className="text-xs font-medium hover:underline">Meu perfil</span></Link>
-                        </motion.div>
-                      </div>
+              {usuario?.id && (
+                <div className="flex items-center gap-4 w-full p-4 pl-3 pb-4">
+                  <div
+                    className={`flex items-center justify-center h-9 min-w-8.5 max-w-full rounded-full gap-4`}
+                  >
+                    <div className="bg-verde rounded-full p-2">
+                      {usuario &&
+                        (usuario.icon ? (
+                          <img
+                            src={urlBase + usuario.icon.file_path}
+                            className={`w-8 h-8 rounded-full`}
+                          />
+                        ) : (
+                          <UserIcon size={18} />
+                        ))}
                     </div>
-                  )
-                }
 
+                    <motion.div
+                      initial={false}
+                      animate={{
+                        opacity: barraLateralAberta ? 1 : 0,
+                        pointerEvents: barraLateralAberta ? "auto" : "none",
+                        transition: {
+                          delay: barraLateralAberta ? 0.3 : 0,
+                          duration: 0.2,
+                          ease: "easeOut",
+                        },
+                      }}
+                      exit={{
+                        display: barraLateralAberta ? "flex" : "none",
+                      }}
+                      style={{
+                        display: barraLateralAberta ? "flex" : "none",
+                      }}
+                      className={`flex flex-col gap-0 min-w-[120px]`}
+                    >
+                      <span className="text-sm font-bold">
+                        {usuario?.username}
+                      </span>
+                      <Link className="h-fit" href="/adm/meu-perfil">
+                        <span className="text-xs font-medium hover:underline">
+                          Meu perfil
+                        </span>
+                      </Link>
+                    </motion.div>
+                  </div>
+                </div>
+              )}
             </div>
           </motion.div>
 
@@ -143,12 +156,10 @@ export default function RootLayout({
             transition={{ type: "spring", stiffness: 300, damping: 30 }}
           >
             <div className="h-5 overflow-y-auto" />
-              {children}
+            {children}
           </motion.div>
-
         </div>
       </div>
-      
     </div>
-  )
+  );
 }
