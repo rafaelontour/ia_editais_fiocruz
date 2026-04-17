9dd4c920fdc991d13c359f836b91b3eca5e358aa
Alana Abreu <149194023+N1nh4@users.noreply.github.com>
2026-04-17 06:53:22 -0300

    abreviando nome para caber na barra lateral docs aqruivados

--- src/core/constants/itensMenu.tsx
+++ src/core/constants/itensMenu.tsx
@@ -46,7 +46,7 @@ const itemsAdm = [
     icon: IdCard,
   },
   {
-    title: "Documentos arquivados",
+    title: "Docs. Arquivados",
     url: "/adm/editais/arquivados",
     icon: InboxIcon,
   }
@@ -69,7 +69,7 @@ const itemsAuditorAnalista = [
     icon: Type,
   },
   {
-    title: "Documentos arquivados",
+    title: "Docs. Arquivados",
     url: "/adm/editais/arquivados",
     icon: InboxIcon,
   }
