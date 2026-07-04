d5f1ee7b67d8d1885ccb08fb89ab70b7a85a605b
Alana Abreu <149194023+N1nh4@users.noreply.github.com>
2026-07-04 09:06:16 -0300

    alteração no span em tip

--- src/app/(paginas)/(adm)/adm/tipificacoes/page.tsx
+++ src/app/(paginas)/(adm)/adm/tipificacoes/page.tsx
@@ -533,10 +533,10 @@ export default function Tipificacoes() {
                     </h2>
                     <div className="flex flex-wrap gap-4 text-sm text-gray-500">
                       <span className="flex justify-center items-center bg-gray-200 px-1 py-0.5 rounded-lg border border-gray-300">
-                        Grupo: {tipificacao.document_group_name ?? "Sem grupo"}
+                        <strong>Grupo:</strong> {tipificacao.document_group_name ?? "Sem grupo"}
                       </span>
                       <span className="flex justify-center items-center bg-gray-200 px-1 py-0.5 rounded-lg border border-gray-300">
-                        Tipo:{" "}
+                        <strong>Tipo:</strong>{" "}
                         {tipificacao.document_group_item_name ?? "Sem tipo"}
                       </span>
                     </div>
